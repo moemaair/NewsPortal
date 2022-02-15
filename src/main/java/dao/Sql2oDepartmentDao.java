821f72d201499305de14a5cfb2bbf4bbd908f071
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-15 10:07:54 +0300

    implemented DepartmentDao methods

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -1,4 +1,55 @@
 package dao;
 
-public class Sql2oDepartmentDao {
+import models.Department;
+import models.News;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+public class Sql2oDepartmentDao implements DepartmentDao {
+    private Sql2o sql2o ;
+
+    public Sql2oDepartmentDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Department department) {
+
+    }
+
+    @Override
+    public void addDepartmentToNews(Department department, News news) {
+
+    }
+
+    @Override
+    public List<Department> getAll() {
+        return null;
+    }
+
+    @Override
+    public Department findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<News> getAllNewsByDepartment(int departmentId) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String description, int numberOfEmployees) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
